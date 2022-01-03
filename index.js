ea63864632f2b17c19eab326c57e6bbe0dc6a407
sairessy <sairessy@gmail.com>
2022-01-03 19:02:13 +0200

    init

---
+++ index.js
@@ -0,0 +1,211 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuCBP3m0B7ANA55MWiXYDOxLSKnNL0uks",
+  authDomain: "mural-44a8b.firebaseapp.com",
+  projectId: "mural-44a8b",
+  storageBucket: "mural-44a8b.appspot.com",
+  messagingSenderId: "1099001930347",
+  appId: "1:1099001930347:web:9830216afee6de7046472e"
+};
+
+// Initialize Firebase
+// const firebase = initializeApp(firebaseConfig);
+
+import Datastore from 'nedb';
+import cookieParser from 'cookie-parser';
+const db = {
+    users: new Datastore('./src/database/users.db'),
+    vitrolas: new Datastore('./src/database/vitrolas.db'),
+    documents: new Datastore('./src/database/documents.db')
+};
+
+db.users.loadDatabase();
+db.vitrolas.loadDatabase();
+db.documents.loadDatabase();
+
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json({limit: '1mb'}));
+app.use(cookieParser());
+app.use(express.static('public'));
+app.listen(PORT, () => console.log(PORT));
+
+app.get('/login', (req, res) => {
+    if (req.cookies.user !== undefined) {
+        res.sendFile(__dirname + '/public/dashboard.html')
+    } else {
+        res.sendFile(__dirname + '/public/login.html');
+    }    
+});
+
+app.get('/signup', (req, res) => {
+    if (req.cookies.user !== undefined) {
+        res.sendFile(__dirname + '/public/dashboard.html')
+    } else {
+        res.sendFile(__dirname + '/public/signup.html');
+    }
+});
+
+app.get('/dashboard', (req, res) => {
+
+    if (req.cookies.user !== undefined) {
+        res.sendFile(__dirname + '/public/dashboard.html')
+    } else {
+        res.sendFile(__dirname + '/public/login.html');
+    }
+});
+
+app.post('/addvitrola', (req, res) => {
+    const data = req.body;
+    db.vitrolas.insert({...data, docs: [], time: Date.now().toString(), 
+        user: req.cookies.user, removed: false}, (err, doc) => console.log(doc));
+    res.json({success: true});
+});
+
+app.post('/adddoc', (req, res) => {
+    const data = req.body;
+
+    // db.vitrolas.update({ _id: data.vitrolaId }, {
+    //     $set: {
+    //         docs: [{...data, time: Date.now().toString()}]
+    //     }
+    // }, { multi: true }, function (err, numReplaced) {
+    //     // console.log(numReplaced)
+    // });
+    const time = Date.now().toString();
+    const newDoc = {
+        ...data, time, removed: false
+    }
+
+    db.vitrolas.update({ _id: data.vitrolaId }, { $push: { docs: newDoc } }, {}, function () {
+        // Now the fruits array is ['apple', 'orange', 'pear', 'banana']
+    });
+
+    db.documents.insert({doc: time, base64: data.file.base64 });
+
+    res.json({})
+});
+
+app.get('/vitrolas', async (req, res) => {
+    db.vitrolas.find({removed: false}, async (err, data) => {
+        let  d = data;
+
+        for (let i = 0; i < data.length; i++) {
+            const v = data[i];
+            v.userOffice = await new Promise((resolve, reject) => {
+                db.users.findOne({_id: v.user}, (err, dt) => {
+                    resolve(dt.office);
+                });
+            }); 
+        }
+
+        res.json(data);
+    });
+});
+
+app.get('/uservitrolas', (req, res) => {
+    db.vitrolas.find({user: req.cookies.user, removed: false}, (err, data) => {
+        console.log(data)
+        res.json(data);
+    });
+});
+
+app.post('/removedoc', (req, res) => {
+    const data = req.body;
+
+    db.vitrolas.findOne({ _id: data.vitrolaId }, (err, d) => {
+        let ds = d.docs;
+        
+        for (let i = 0; i < d.docs.length; i++) {
+            const e = d.docs[i];
+            if(e.time == data.doc) {
+                ds[i].removed = true;
+                break;
+            }
+        }
+        
+        db.vitrolas.update({ _id: data.vitrolaId }, {
+            $set: {
+                docs: ds
+            }
+        }, { multi: true }, function (err, numReplaced) {
+            // console.log(numReplaced)
+        });
+    });
+  
+    res.json({})
+});
+
+app.post('/removevitrola', (req, res) => {
+    const data = req.body;
+    console.log(req.body);
+    db.vitrolas.update({ _id: data.vitrolaId }, {
+        $set: {
+            removed: true
+        }
+    }, { multi: true }, function (err, numReplaced) {
+        // console.log(numReplaced)
+    });
+    res.json({})
+});
+
+app.get('/logout', (req, res) => {
+    res.clearCookie('user', {})
+    res.redirect('/login')
+});
+
+app.post('/login', (req, res) => {
+    const data = req.body;
+    db.users.findOne({email: data.email, pass: data.pass}, (err, d) => {
+        
+        if(d != null) {
+            res.cookie('user', d._id, {});
+        }
+
+        res.json({success: d != null});
+    });
+});
+
+app.post('/signup', (req, res) => {
+    const data = req.body;
+    db.users.insert(data, (err, doc) => console.log(doc));
+    res.json({});
+});
+
+app.get('/doc/:id', (req, res) => {
+    const id = req.params.id;
+    db.documents.findOne({doc: id}, (err, data) => {
+
+        if(data != null) {
+            res.send(`
+                <div style="background: #fff; height: calc(100vh - 20px); margin: 0; padding: 0;
+                    display: flex; justify-content: center; align-items: center;"
+                >
+                    <a 
+                        href="${data.base64}" download="${data.doc}"
+                        style="
+                            padding: 15px; text-decoration: none; color: indigo;
+                            background: #2bccb1; color: #fff; font-family: arial; display: block;
+                            width: 280px; border-radius: 2px; text-align: center;
+                        "
+                    >
+                        Baixar
+                    </a>
+                <div>
+            `);
+        } else {
+            res.send('File not found');
+        }
+    });
+});
---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+	docsExtensions: [
+		{id: '', value: '', icon: '' },
+		{id: 0, value: 'doc', icon: '' },
+		{id: 1, value: 'pdf', icon: '' },
+		{id: 2, value: 'xls', icon: '' },
+		{id: 3, value: 'png', icon: '' },
+		{id: 4, value: 'jpg', icon: '' },
+		{id: 5, value: 'jpeg', icon: '' }
+	]
+};
